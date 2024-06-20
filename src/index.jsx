508c62c7348d35718a677342845626d126bf1d48
uzdik <uzdik.kz@gmail.com>
2024-06-20 20:20:31 +0900

    Update CSS index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./style/index.css";
+import "https://uzdik.github.io/assets/css/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
